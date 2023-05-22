7359cf5e0df0830ba4d879d28c61af3c489d1164
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-22 17:06:33 +0430

    The BorrowBook method in the LibraryRepository class was added.

---
+++ LibrarySystem/Repository/LibraryRepository.cs
@@ -0,0 +1,50 @@
+﻿using LibrarySystem.Abstraction;
+using LibrarySystem.Entities;
+using System.Globalization;
+
+namespace LibrarySystem.Repository
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        private IStorage _storage;
+
+        public LibraryRepository(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public bool BorrowBook(string username, string bookName)
+        {
+            var requestedBook = _storage.GetAvailableBooks().
+                                    FirstOrDefault(u => u.bookName == bookName);
+
+            if (requestedBook != null)
+            {
+                requestedBook.IsBorrowed = true;
+                requestedBook.BorrowerUsername = username;
+                requestedBook.BorrowDate = DateTime.Today;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public List<Book> GetBooksList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetBorrowedBooksList(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReturnBook(string username, Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
